05a56738d0c10b30dce9b0646a003b1c29b9f5cb
CicadaCode <lengthwise2424@gmail.com>
2025-05-09 11:49:30 -0500

    SonarTest

--- src/main/java/com/exam/ms_ordenes/controller/OrdenController.java
+++ src/main/java/com/exam/ms_ordenes/controller/OrdenController.java
@@ -5,7 +5,6 @@ import com.exam.ms_ordenes.entity.Orden;
 import com.exam.ms_ordenes.service.OrdenService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
